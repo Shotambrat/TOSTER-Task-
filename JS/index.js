909051b6ea727168558583fbc2c62eabadf91dd2
Molly <dudumikah@gmail.com>
2023-09-21 13:06:55 +0500

    CHECK SLIDER

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,30 @@
+const finaleTime = new Date("January 1, 2024 00:00:00").getTime();
+
+const timer = () =>{
+    const currentTime = new Date().getTime();
+    let difference = finaleTime - currentTime;
+
+    let hours = Math.floor(difference % (1000 * 60 * 60 *24) / (1000 * 60*60));
+    let minutes = Math.floor(difference % (1000*60*60)/ (1000 *60));
+    let seconds = Math.floor(difference % (1000*60) / 1000);
+
+    hours <= 9 ? hours = `0${hours}` : hours;
+    minutes <= 9 ? minutes = `0${minutes}` : minutes;
+    seconds <= 9 ? seconds = `0${seconds}` : seconds;   
+
+    document.querySelector('#hours').textContent = hours;
+    document.querySelector('#minutes').textContent = minutes;
+    document.querySelector('#seconds').textContent = seconds;
+}
+timer();
+// Calling the function every 1000 milliseconds.
+setInterval(timer, 1000);
+
 const caruselElem = document.querySelectorAll('.carusel-elem__item');
 
-caruselElem.forEach((el, index) => {
-    el.setAttribute('data-index', index)
+caruselElem.forEach(el => {
+
+    el.addEventListener('click', (e) => {
+        document.querySelector('.carusel__preview-img').src = el.getAttribute('src');
+    })
 });
